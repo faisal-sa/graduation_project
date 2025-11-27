c6361d0281f3ca4ce8f3f47dd988088ee1681533
faisal-alfaifi-work <faisal.alfaifi.work@gmail.com>
2025-11-27 13:59:12 +0300

    exports file

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,42 +1,4 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_it/get_it.dart';
-import 'package:go_router/go_router.dart';
-import 'package:graduation_project/core/di/service_locator.dart';
-import 'package:graduation_project/features/CRinfo/cr_info_page.dart';
-import 'package:graduation_project/features/company_portal/presentation/blocs/bloc/company_bloc.dart';
-import 'package:graduation_project/features/company_portal/presentation/screens/advanced_searchpage.dart';
-import 'package:graduation_project/features/company_portal/presentation/screens/company_bookmarks_page.dart';
-import 'package:graduation_project/features/company_portal/presentation/screens/company_qr_page.dart';
-
-import 'package:graduation_project/features/company_portal/presentation/screens/company_search_page.dart';
-import 'package:graduation_project/features/company_portal/presentation/screens/company_settings_page.dart';
-import 'package:graduation_project/features/company_portal/presentation/screens/complete_company_profile_page.dart';
-import 'package:graduation_project/features/company_portal/presentation/screens/payment_page.dart';
-
-import 'package:graduation_project/features/individuals/chat/presentation/pages/chats_tab.dart';
-import 'package:graduation_project/features/individuals/features/about_me/presentation/cubit/about_me_cubit.dart';
-import 'package:graduation_project/features/individuals/features/about_me/presentation/pages/about_me_page.dart';
-import 'package:graduation_project/features/individuals/features/basic_info/presentation/cubit/basic_info_cubit.dart';
-import 'package:graduation_project/features/individuals/features/basic_info/presentation/pages/basic_info_page.dart';
-import 'package:graduation_project/features/individuals/features/certifications/presentation/cubit/list/certification_list_cubit.dart';
-import 'package:graduation_project/features/individuals/features/certifications/presentation/pages/certification_page.dart';
-import 'package:graduation_project/features/individuals/features/education/presentation/cubit/list/education_list_cubit.dart';
-import 'package:graduation_project/features/individuals/features/education/presentation/pages/education_page.dart';
-import 'package:graduation_project/features/individuals/features/job_preferences/presentation/pages/job_preferences_page.dart';
-import 'package:graduation_project/features/individuals/features/skills_languages/presentation/pages/skills_page.dart';
-import 'package:graduation_project/features/individuals/features/work_experience/presentation/cubit/list/work_experience_list_cubit.dart';
-import 'package:graduation_project/features/individuals/features/work_experience/presentation/pages/work_experience_list_page.dart';
-import 'package:graduation_project/features/individuals/insights/presentation/pages/insights_tab.dart';
-import 'package:graduation_project/features/individuals/profile/presentation/cubit/profile_cubit.dart';
-import 'package:graduation_project/features/individuals/profile/presentation/pages/profile_tab.dart';
-import 'package:graduation_project/features/individuals/navigation/pages/individuals_home_page.dart';
-import 'package:graduation_project/features/auth/presentation/pages/login_page.dart';
-import 'package:graduation_project/features/auth/presentation/pages/signup_page.dart';
-import 'package:graduation_project/features/auth/presentation/pages/otp_verification_page.dart';
-
-import 'package:flutter/material.dart';
-import 'package:graduation_project/features/payment/pay_page.dart';
-import 'package:graduation_project/features/shared/user_cubit.dart';
+import 'package:graduation_project/core/exports/app_exports.dart';
 
 // keep it here for now
 final _rootNavigatorKey = GlobalKey<NavigatorState>();
@@ -197,6 +159,7 @@ final GoRouter router = GoRouter(
         ),
       ],
     ),
+
     // -------------------- COMPANY PORTAL FLOW --------------------
     GoRoute(
       path: '/company/complete-profile',
